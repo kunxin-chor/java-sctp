7d43bbba4c6659058ae328ea40413ba864ca8319
kunxin-chor <chorkunxin@yahoo.com>
2025-04-14 12:53:23 +0000

    Added values and variables

---
+++ 02-variables/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    // All Java program stsrt from a `main` function
+    public static void main(String[] args) {
+        // VALUES = different kind of data that the programming language knows
+        // The type of variable must be defined when we create a new variable
+        // Uses double quotes
+        // JS: let something = "the quick brown fox";
+        // Strings in Java must be double quoted
+        String something = "The quick brown fox jumps over the lazy dog";
+
+        // Numbers in Java - state THE type of number
+        // integers
+        int secretOfLife = 42;
+        // double precision (aka double) is the default -- can store twice the precison
+        // as a float
+        double pi = 3.14;
+        // floating point (aka float) -- having double precision is overkill sometimes
+        float gstPercentage = 0.09f;
+
+        // There are different type of intgers
+        byte myWallet = 126;  // byte at most store between -127 to 128
+
+        // boolean variables are either true or false
+        boolean isRaining = true;
+
+        // char data type
+        // 'char' is for 'character'
+        // A string consists of many characters
+        // Therefore 1 char variable represents one character in a string
+        // Uses single quotes
+        char alphabet = 'Z';
+    }
+}
